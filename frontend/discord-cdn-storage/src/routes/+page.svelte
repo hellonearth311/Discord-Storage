9200528774097a965943c001db0ce581b8852638
swaritnarang <naranswarit02@gmail.com>
2026-01-03 03:08:47 -0600

    made a frontend that kinda sucks but like who cares

---
+++ frontend/discord-cdn-storage/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<h1 id="title">Discord CDN Storage</h1>
+
+<div class="div_container">
+    <div class="div_content">
+        <img src="upload.png" alt="upload arrow" id="uploadArrow">
+        <button id="uploadButton">upload!</button>
+        <p id="fileInfoText">size limit of 10mb please! supported file types: <b>png, jpeg, svg, jpg</b></p>
+    </div>
+</div>
+
+
+
