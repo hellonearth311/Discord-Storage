e71f65eae5d19516b9c744a465a1e13ff2260480
swaritnarang <naranswarit02@gmail.com>
2026-01-03 01:40:39 -0600

    sends images now

--- bot.py
+++ bot.py
@@ -1,7 +1,10 @@
 # imports
 import os
 import discord
+import random
+
 from dotenv import load_dotenv
+from discord.ext import tasks
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -11,8 +14,22 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+@tasks.loop(seconds=2.0)
+async def send_every_second():
+    imageToSend = random.randint(1, 3)
+
+    imgChannel = client.get_channel(1456908142615990315)
+    img = discord.File(f"files/img{imageToSend}.jpg")
+    
+    if imgChannel:
+        await imgChannel.send("testing123 hehe", file=img)
+
 @client.event
 async def on_ready():
+    # main code here
     print(f'{client.user} has connected to Discord!')
+
+    if not send_every_second.is_running():
+        send_every_second.start()
 
 client.run(TOKEN)
